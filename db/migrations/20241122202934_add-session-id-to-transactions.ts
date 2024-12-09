086495cf1aa417b58a052d8efe638c8d341c2811
Gabriel <gabrielzmendes1@gmail.com>
2024-12-09 19:34:54 -0300

    Utilizando cookies no Fastify

--- db/migrations/20241122202934_add-session-id-to-transactions.ts
+++ db/migrations/20241122202934_add-session-id-to-transactions.ts
@@ -6,6 +6,8 @@ export async function up(knex: Knex): Promise<void> {
   });
 }
 
-export async function down(Knex: Knex): Promise<void> {
-  await Knex.schema.dropTable('session_id');
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    table.dropColumn('session_id');
+  });
 }
